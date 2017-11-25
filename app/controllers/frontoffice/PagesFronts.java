403b2089a2ff2c4df5c2b6bf42c7e1baa7a0551b
Julien Custoja <julien.custoja@gmail.com>
2017-11-25 21:52:11 +0100

    Add 404 for missing webpages

--- app/controllers/frontoffice/PagesFronts.java
+++ app/controllers/frontoffice/PagesFronts.java
@@ -8,12 +8,13 @@ import play.mvc.Controller;
 public class PagesFronts extends Controller{
 
     public static void index(String slug) {
-        System.out.println("ICICICICICICICI");
         PagesFront page = PagesFront.find("slug", slug).first();
 
         if(page != null) {
             render(template("frontoffice/PagesFronts/pageFront.html"), page);
         }
+
+        notFound();
     }
 
 }
